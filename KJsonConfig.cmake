69af27f9a2f10d361c24b34d17bc6963b5af53fb
KooBH <ninie4409@sogang.ac.kr>
2021-10-29 19:29:13 +0900

    cmake

---
+++ KJsonConfig.cmake
@@ -0,0 +1,27 @@
+################################################################
+#     CMake file for using KJsonConfig  as submodule           #
+# 															   #
+################################################################
+
+list(APPEND SRC_Qt_MOC
+	${CMAKE_CURRENT_LIST_DIR}/KJsonConfig.h
+	${CMAKE_CURRENT_LIST_DIR}/ParamCheckBox.h
+	${CMAKE_CURRENT_LIST_DIR}/ParamComboBox.h.h
+	${CMAKE_CURRENT_LIST_DIR}/ParamLineEdit.h
+	
+)
+
+list(APPEND SRC_Qt
+	${CMAKE_CURRENT_LIST_DIR}/KJsonConfig.cpp
+	${CMAKE_CURRENT_LIST_DIR}/ParamCheckBox.cpp
+	${CMAKE_CURRENT_LIST_DIR}/ParamComboBox.cpp
+	${CMAKE_CURRENT_LIST_DIR}/ParamLineEdit.cpp
+)
+
+list(APPEND SRC_Qt
+)
+	
+list(APPEND INCL
+	${CMAKE_CURRENT_LIST_DIR}/lib
+)
+
